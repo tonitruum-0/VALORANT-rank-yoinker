a56559d170a532cf22330adf1b03b5776c2cf406
tonitruum <104451675+tonitruum-0@users.noreply.github.com>
2025-06-10 01:03:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -429,9 +429,9 @@ try:
 
                         if player["PlayerIdentity"]["Incognito"]:
                             print(
-                                player["CharacterID"]
+                                str(agent_dict.get(player["CharacterID"].lower()))
                                 + " | "
-                                + str(agent_dict.get(player["CharacterID"].lower()))
+                                + names[player["Subject"]]
                             )
                             Namecolor = colors.get_color_from_team(
                                 player["TeamID"],
