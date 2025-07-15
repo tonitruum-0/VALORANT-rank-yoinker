c5c57898907d61a3db2fd696381c3b59319abea3
tonitruum <104451675+tonitruum-0@users.noreply.github.com>
2025-07-15 16:50:01 -0400

    fixed

--- src/constants.py
+++ src/constants.py
@@ -81,10 +81,6 @@ AGENTCOLORLIST = {
 }
 
 
-GAMEPODS = requests.get("https://valorant-api.com/internal/locres/en-US").json()[
-    "data"
-]["UI_GamePodStrings"]
-
 symbol = "■"
 PARTYICONLIST = [
     color(symbol, fore=(227, 67, 67)),
